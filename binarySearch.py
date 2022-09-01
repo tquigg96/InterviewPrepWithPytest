18dd5a19dd3d1c17e8e5fdfc3d8a4394f0a6854f
Travis Quigg <t.quigg.hulsey@outlook.com>
2022-08-31 17:23:07 -0700

    needed sort for binary search to work

--- binarySearch.py
+++ binarySearch.py
@@ -1,14 +1,13 @@
 def search(nums, target):
+
+    nums.sort()
     
     right = len(nums) - 1
     left = 0
-
-    mid = (len(nums)-1) //2
-    print(mid)
         
 
     if len(nums) == 1 or nums[0] == target:
-        return 0
+        return True
         
 
     while(left <= right):
@@ -16,21 +15,18 @@ def search(nums, target):
             mid = left + (right-left) //2 #half it
 
             if nums[mid] == target:
-                return mid
+                return True
             elif nums[mid] > target:
                 right = mid - 1
             elif nums[mid] < target:
-                left = mid + 1
-        
-        
-                
+                left = mid + 1            
                         
-    return -1
+    return False
 
 
 nums = [4,5,6,7,0,1,2]
 
-val = search(nums, 6)
+val = search(nums, 1)
 print(val)
 
     
